348eacfde616d09fe71b746c41e556461e5327d3
MariaProcopii <mari.procopii@gmail.com>
2022-10-06 08:53:45 +0300

    change in Main.java, Cleaner.java, Player.java

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+
+        int nrOfPlayers = 12;
+        ArrayList<Player> customersList = new ArrayList<>();
+        for(int i = 0; i < nrOfPlayers; i++){
+            customersList.add(new Player());
+        }
+
+        for(int i = 0; i < customersList.size(); i++){
+            System.out.println("\n====================================");
+            System.out.println("Restaurant Simulation Nr: [" + (i + 1) + "]");
+            System.out.println("====================================\n");
+
+            Player player = customersList.get(i);
+            Table table = new Table(i, player);
+            Admin admin = new Admin(player, table, i);
+            Menu menu = new Menu(player);
+            Meal meal = new Meal(player, menu);
+            Waiter waiter = new Waiter(menu, meal, player);
+            Cook cook = new Cook(meal, waiter, player, table);
+            Cleaner cleaner = new Cleaner(player, table);
+            Deck deck = new Deck();
+            Barman barman = new Barman(player, deck);
+
+            player.greeting();
+            admin.greeting();
+            if(admin.provideTable(customersList) == 0){
+                admin.printStatistic();
+                break;
+            }
+            table.qualityCheck();
+            menu.qualityCheck();
+            waiter.greeting();
+            waiter.takeOrder();
+            cook.qualityCheck();
+            meal.qualityCheck();
+            cook.cooking();
+            cleaner.cleanTable();
+            barman.greeting();
+            waiter.bringBill();
+            player.goodBye();
+            admin.printStatistic();
+        }
+    }
+}
---
+++ Waiter.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.util.Random;
+
+public class Waiter extends Human{
+
+    private final Menu menu;
+    private final Meal meal;
+    private final Player player;
+    public int bill;
+    public int polite;                                     // 1 = bad, 2 = good, 3 = nice;
+
+    public Waiter(Menu menu, Meal meal, Player player){
+        this.menu = menu;
+        this.meal = meal;
+        this.player = player;
+        this.polite = random.nextInt(3) + 1;
+    }
+
+    public void greeting(){
+        switch (polite) {
+            case 3 -> {
+                System.out.println("Waiter: Good morning. " + player.name +
+                        " My name is " + name + ". What do you want to order ?");
+                player.opinion += 2;
+                setReputation(2);
+            }
+            case 2 -> {
+                System.out.println("Waiter: Hallo. What do you want to order ?");
+                player.opinion++;
+                setReputation(1);
+            }
+            case 1 -> {
+                System.out.println("Waiter: Are you ready or not to make an order ?!");
+                player.opinion--;
+                setReputation(-2);
+            }
+        }
+    }
+
+    public void speak(){
+        int phrase = random.nextInt(phrases.length);
+        System.out.println("Waiter: " + phrases[phrase] + ".\nWaiter: I should stop talking to myself.");
+    }
+
+    public void goodBye(){
+        if(polite == 1){
+            System.out.println("Waiter: stop yelling at me !");
+            setReputation(-2);
+        }
+    }
+
+    public void takeOrder(){
+        meal.pickFood();
+        meal.stopListCheck();
+
+        System.out.println("Player: " + player.name + " ordered "
+                           + meal.getFood() + " with " + meal.getSupply() + ".");
+        System.out.println("Waiter: Thanks " + player.name + ". Your order will be ready as soon as possible.");
+    }
+
+    public void bringBill(){
+        menu.createFoodMenu();
+        menu.createSuppMenu();
+        int bill1 = menu.getFoodWithPrice().get(player.getFood());
+        int bill2 = menu.getSuppliesWithPrice().get(player.getSupply());
+        this.bill = bill1 + bill2;
+        player.pay(bill);
+        System.out.println("Waiter: Your bill, " + player.name
+                           + ", is " + bill + " $");
+
+        System.out.println("Player: Now balance of my card is: " + player.moneyAmount() + " $");
+    }
+}
